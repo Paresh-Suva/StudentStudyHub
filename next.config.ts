2184ef10f164425e730c3715b853dc8ec2377af4
Paresh <paresho7suva@gmail.com>
2025-12-16 00:29:42 +0530

    Fix NextConfig types

--- next.config.ts
+++ next.config.ts
@@ -5,10 +5,17 @@ const nextConfig: NextConfig = {
     remotePatterns: [
       {
         protocol: "https",
+        hostname: "utfs.io",
+        pathname: "**",
+      },
+      {
+        protocol: "https",
         hostname: "img.clerk.com",
+        pathname: "**",
       },
     ],
   },
+  // We will handle ESLint and TS errors differently to satisfy the type checker
 };
 
 export default nextConfig;
